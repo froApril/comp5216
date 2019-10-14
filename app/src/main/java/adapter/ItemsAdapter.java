1bbd622caf77700d749f10681a4f577476894c5f
Simon <syao7430@uni.sydney.edu.au>
2019-10-15 02:42:04 +1100

    Simon finish

--- app/src/main/java/adapter/ItemsAdapter.java
+++ app/src/main/java/adapter/ItemsAdapter.java
@@ -9,52 +9,47 @@ import com.bumptech.glide.Glide;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import Beans.Items;
 import mobileproject.au.edu.sydney.comp5216.mobileproject.R;
 
 public class ItemsAdapter extends BaseAdapter {
-    public Context context;
-    public List<Items> list;
+    private Context context;
+    private ArrayList<Items> list;
     private int num1;
 
-    public ItemsAdapter(Context context, List<Items> list) {
+    public ItemsAdapter(Context context, ArrayList<Items> list) {
         this.context = context;
         this.list = list;
     }
     @Override
     public int getCount() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return list.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(final int i, View view, ViewGroup viewGroup) {
-        final ViewHolder viewHolder ;
-        if(view == null){
-            view= LayoutInflater.from(context).inflate(R.layout.activity_item,null);
-            viewHolder=new ViewHolder();
-            viewHolder.titleView = (TextView) view.findViewById(R.id.restaurant_item_name);
-            viewHolder.descriptionView = (TextView) view.findViewById(R.id.restaurant_item_description);
-            viewHolder.priceView= (TextView) view.findViewById(R.id.restaurant_item_price);
-            viewHolder.image= (ImageView) view.findViewById(R.id.restaurant_item_image);
-            viewHolder.minus= (ImageView) view.findViewById(R.id.id_lv_minus);
-            viewHolder.amountView= (TextView) view.findViewById(R.id.id_lv_num);
-            viewHolder.add= (ImageView) view.findViewById(R.id.id_lv_add);
-            view.setTag(viewHolder);
-        }else{
-            viewHolder= (ViewHolder) view.getTag();
-        }
+        final ViewHolder viewHolder=new ViewHolder(); ;
+        view= LayoutInflater.from(context).inflate(R.layout.activity_item,null);
+        viewHolder.titleView = (TextView) view.findViewById(R.id.restaurant_item_name);
+        viewHolder.descriptionView = (TextView) view.findViewById(R.id.restaurant_item_description);
+        viewHolder.priceView= (TextView) view.findViewById(R.id.restaurant_item_price);
+        viewHolder.image= (ImageView) view.findViewById(R.id.restaurant_item_image);
+        viewHolder.minus= (ImageView) view.findViewById(R.id.id_lv_minus);
+        viewHolder.amountView= (TextView) view.findViewById(R.id.id_lv_num);
+        viewHolder.add= (ImageView) view.findViewById(R.id.id_lv_add);
+        view.setTag(viewHolder);
 
         viewHolder.titleView.setText(""+list.get(i).getTitle());
         viewHolder.descriptionView.setText(""+list.get(i).getDescription());
@@ -71,6 +66,8 @@ public class ItemsAdapter extends BaseAdapter {
                 if(num1 <=0){//逻辑判断，如果减为0的话，就一直为0，要不然就是符号了。
                     num1 =0;
                 }
+                viewHolder.amountView.setText(num1 +"");
+                list.get(i).setNum(num1);
                 onMyItemClickListener.onBtnItemListener(viewHolder.amountView,i, num1);//使用刚才自定义的接口。
             }
         });
@@ -80,6 +77,7 @@ public class ItemsAdapter extends BaseAdapter {
                 num1 = list.get(i).getNum();//获取到（加号）改动过的 数量
                 num1++;
                 viewHolder.amountView.setText(num1 +"");
+                list.get(i).setNum(num1);
                 onMyItemClickListener.onBtnItemListener(viewHolder.amountView,i, num1);
             }
         });
--- app/src/main/java/mobileproject/au/edu/sydney/comp5216/mobileproject/OrderActivity.java
+++ app/src/main/java/mobileproject/au/edu/sydney/comp5216/mobileproject/OrderActivity.java
@@ -1,27 +1,129 @@
 package mobileproject.au.edu.sydney.comp5216.mobileproject;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Adapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import Beans.Items;
 import adapter.ItemsAdapter;
 
 public class OrderActivity extends AppCompatActivity {
+    private String storeName = "KFC";
+    private String TAG = "Simon";
+    private ListView itemsList;
+    private ItemsAdapter itemsAdapter;
+    ArrayList<Items> menuList;
+    private DatabaseReference mDatabase =  FirebaseDatabase.getInstance().getReference();
 
-//    ListView itemsList;
-//    ItemsAdapter itemsAdapter;
-//    ArrayList order;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-//        order = new ArrayList();
-//        itemsList = (ListView)findViewById(R.id.list_item);
-//        itemsAdapter = new ItemsAdapter(this,order);
-//        itemsList.setAdapter(itemsAdapter);
+        setAllCommentsList(storeName);
 
     }
+    public void setAllCommentsList(String storename){
+        mDatabase.child("restaurants").child(storename)
+                .child("menu")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        menuList = new ArrayList<Items>();
+                        for (DataSnapshot alert:dataSnapshot.getChildren()) {
+
+                            Gson gson = new Gson();
+                            String s = gson.toJson(alert.getValue());
+                            menuList.add(gson.fromJson(s,Items.class));
+                        }
+                        setListView(menuList);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    private void setListView(final ArrayList<Items> menuList){
+        itemsList = findViewById(R.id.list_item);
+        itemsAdapter = new ItemsAdapter(this,menuList);
+        itemsAdapter.setOnMyItemClickListener(new ItemsAdapter.OnMyItemClickListener() {
+            @Override
+            public void onBtnItemListener(TextView tv, int i, int num) {
+                menuList.get(i).setNum(num);
+            }
+        });
+        System.out.println(menuList.size());
+        itemsList.setAdapter(itemsAdapter);
+    }
+
+    public void MakeOrder(View view){
+        makeOrder(menuList,storeName);
+//        Intent intent = new Intent();
+//        startActivity(intent);
+    }
+
+    private void makeOrder(ArrayList<Items> order,String storeName){
+        HashMap<String, Object> Makeorder = new HashMap<>();
+        String s= "";
+        Double price=0.0;
+        for(int x = 0; x <order.size(); x++){
+
+            if(order.get(x).getNum()>0){
+                s = s + order.get(x).getTitle()+": "+order.get(x).getNum()+", ";
+                price = price + order.get(x).getNum()*order.get(x).getPrice();
+            }
+
+        }
+        Makeorder.put("client_id", "Test User");
+        Makeorder.put("item_list", s);
+        Makeorder.put("total_price", price);
+        mDatabase.child("users").child("store")
+                .child(storeName).child("order").child("order2")
+                .setValue(Makeorder)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.e("ok!!","insert");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("problem", e + "");
+                    }
+                });
+    }
+
 }
