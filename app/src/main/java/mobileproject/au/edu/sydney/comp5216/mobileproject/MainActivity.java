737039acad43c3ffde219e8404b1502301add58d
Superbody <329002312@qq.com>
2019-10-24 16:48:10 +1100

    DELETE..

--- app/src/main/java/mobileproject/au/edu/sydney/comp5216/mobileproject/MainActivity.java
+++ app/src/main/java/mobileproject/au/edu/sydney/comp5216/mobileproject/MainActivity.java
@@ -1,25 +1,15 @@
 package mobileproject.au.edu.sydney.comp5216.mobileproject;
 
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import Login.EmailPasswordActivity;
-import Login.FacebookLoginActivity;
 import Login.ForgotPassword;
-import Login.GoogleSignInActivity;
-import Login.PasswordlessActivity;
 
 
 public class MainActivity extends AppCompatActivity {
